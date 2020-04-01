c91fb0c0f224b44ac867601270e07b1a8d9d718f
Anil Kapoor <anilkapoor@Anils-MacBook-Air.local>
2020-04-01 11:19:05 +0530

    Change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port 		= process.env.PORT || 3000;
 
 app.use(cors());
 
-let clients = [];
+let clients = 0;
 
 io.on('connection', function(socket){
 	console.log('New Socket Connected with =>'+ socket.id);
@@ -53,29 +53,30 @@ io.on('connection', function(socket){
 		socket.join(roomName);
 	});
 
-	socket.on('NewClient', function(){
-		if(clients.length < 2) {
-			if(clients.length == 1){
-				this.emit('CreatePeer')
+	socket.on('newClient', function(){
+		if(clients < 2) {
+			if(clients == 1){
+				this.emit('createPeer')
 			}
 		} else {
-			this.emit('SessionActive')
+			this.emit('sessionActive')
 		}
-		clients.push(socket.id);
+		clients++;
 	});
-	socket.on('Offer', SendOffer)
-	socket.on('Answer', SendAnswer)
+
+	socket.on('offer', SendOffer)
+	socket.on('answer', SendAnswer)
 	socket.on('disconnect', function(reason){
 		console.log("A use has been disconnected "+ reason);
 	});
 });
 
 function SendOffer(offer){
-	this.broadcast.emit('BackOffer', offer)
+	this.broadcast.emit('backOffer', offer)
 }
 
 function SendAnswer(data){
-	this.broadcast.emit('BackAnswer', data);
+	this.broadcast.emit('backAnswer', data);
 }
 
 http.listen(port, () => console.log(`Active on ${port} port`));
